d6732f5fc637bc6d53ca1f7aa63304562648442d
DavidSoderqvist <davidsoder_@hotmail.com>
2026-01-26 14:41:53 +0100

    Skapade interfacet Hittable

--- src/hittable/HitRecord.java
+++ src/hittable/HitRecord.java
@@ -1,6 +1,12 @@
 package hittable;
 import math.Vec3;
 
+/**
+ * Records information about a ray-object intersection.
+ * 
+ * This class stores the point of intersection, the normal at that point,
+ * and the parameter t along the ray where the intersection occurs.
+ */
 public class HitRecord {
     public Vec3 p;
     public Vec3 normal;
---
+++ src/hittable/Hittable.java
@@ -0,0 +1,15 @@
+package hittable;
+import math.Ray;
+
+public interface Hittable {
+    /**
+     * Determines if a ray hits the object within a specified range.
+     *
+     * @param r      The ray to test for intersection.
+     * @param tMin   The minimum t value to consider for a hit.
+     * @param tMax   The maximum t value to consider for a hit.
+     * @param rec    A HitRecord to store intersection details if a hit occurs.
+     * @return       True if the ray hits the object, false otherwise.
+     */
+    boolean hit(Ray r, double tMin, double tMax, HitRecord rec);
+}
